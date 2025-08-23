b70febc884bc4003ff9e1df7932ee44173047f7f
user.email "nimeshsolanki106@gmail.com <nimeshsolanki106@gmail.com>
2025-08-23 16:49:42 +0530

    set it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Send, Moon, Sun } from "lucide-react";
 import { io } from "socket.io-client";
 
 // Connect to backend Socket.IO server
-const socket = io("http://localhost:3000"); // change to your backend URL if needed
+const socket = io(import.meta.env.VITE_SOCKET_URL);
 
 const App = () => {
   const [messages, setMessages] = useState([
